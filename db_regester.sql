891fdd3558504c29d5e7a92785c99efb1cbaa995
divyanshmaurya <divyansh.ku@gmail.com>
2017-01-17 14:32:44 +0530

    Add files via upload

---
+++ db_regester.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 17, 2017 at 09:15 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sample`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `db_regester`
+--
+
+CREATE TABLE `db_regester` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `db_regester`
+--
+
+INSERT INTO `db_regester` (`username`, `password`) VALUES
+('Basu', 'asd'),
+('coconut', 'coco'),
+('Kartiken', 'kt'),
+('Mishra', 'Mishri'),
+('test', 'test'),
+('Tom', '13'),
+('tony', 'stark');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `db_regester`
+--
+ALTER TABLE `db_regester`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
